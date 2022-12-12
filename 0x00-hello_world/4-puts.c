e4408260ecc8cc9d6d1f3c2bd8c1dc781db4eb1f
Oloo Walter <walter_oloo@students.uonbi.ac.ke>
2022-12-12 12:00:05 -0800

    c is hard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /* main indicating entry point */
 int main(void)
+/* function to print to stdio */
 {
 puts("\"Programming is like building a multilingual puzzle");
+/* with a return value */
 return (0);
 }
