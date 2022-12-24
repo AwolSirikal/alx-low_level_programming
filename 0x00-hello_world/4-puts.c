aa461746b80dd300eee36726c7d4d12e5082778e
Oloo Walter <walter_oloo@students.uonbi.ac.ke>
2022-12-24 01:22:02 -0800

    getting the hang of it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /* main indicating entry point */
 int main(void)
-/* function to print to stdio */
 {
-puts("\"Programming is like building a multilingual puzzle");
-/* with a return value */
-return (0);
+  puts("\"Programming is like building a multilingual puzzle puzzle");
+  return (0);
 }
